60734e9f610a72b54b42b2cec12fe3d0371ee1bd
fl0wZyy <mpt319@stern.nyu.edu>
2021-11-22 20:42:09 +0100

    Fixed Transparency Issues

--- scripts/image_generation.py
+++ scripts/image_generation.py
@@ -24,7 +24,7 @@ def GetBitmap():
 
 
 def CreateCanvas(canvas_size, canvas_bg_color):
-    canvas_element = Image.new("RGB", canvas_size, canvas_bg_color)
+    canvas_element = Image.new("RGBA", canvas_size, canvas_bg_color)
     return canvas_element
 
 
@@ -32,9 +32,10 @@ def DrawLayer(canvas_element, bitmap, nft_id):
     draw = ImageDraw.Draw(canvas_element)
     for x in range(0, 1000, 10):
         for y in range(0, 1000, 10):
-            color = ImageColor.getcolor("#" + str(bitmap[int(x / 10)][int(y / 10)]), "RGB")
-            draw.rectangle((x, y, x + 10, y + 10), color)
-    canvas_element.save(f'./assets/layer/{nft_id}.png')
+            if bitmap[int(x / 10)][int(y / 10)] != "000000":
+                color = ImageColor.getcolor("#" + str(bitmap[int(x / 10)][int(y / 10)]), "RGB")
+                draw.rectangle((x, y, x + 10, y + 10), color)
+    canvas_element.save(f'./assets/layer/{nft_id}.png', 'PNG')
 
 
 def Composite(nft_id):
@@ -63,7 +64,7 @@ def Upload(nft_id):
 
 def main():
     nft_id, bitmap = GetBitmap()
-    canvas = CreateCanvas((1000, 1000), (255, 255, 255))
+    canvas = CreateCanvas((1000, 1000), (255, 0, 0, 0))
     DrawLayer(canvas, bitmap, nft_id)
     Composite(nft_id)
     Upload(nft_id)
